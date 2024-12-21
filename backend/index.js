fbea5a9847bccb752c76efecbc7e3af199585d4f
Harshit Behl <harshitbehl671@gmail.com>
2024-12-21 14:49:07 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const  express  =  require('express');
+const app =  express();
+require('dotenv').config();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.get('/',(req,res) => {
+    res.send('hi');
+})
+
+app.listen(process.env.PORT);
